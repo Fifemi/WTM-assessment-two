a9a335d81aaf817e4d86c046043b4ba54b748ea2
fifemi <fifemiipaye2@gmail.com>
2023-07-20 21:14:50 +0100

    created my second assessment using html and css

---
+++ main.js
@@ -0,0 +1,9 @@
+//this is my first javascript code
+console.log("hello world!");
+
+//variables 
+let interestRate = 0.3;
+interestRate = 45;
+console.log(interestRate);
+
+let selectedColor = null;
